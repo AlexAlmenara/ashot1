eceb0cafc4ffadd0eacf88f639a3a1bbb59764a1
alexalmenara89@gmail.com <alexalmenara89@gmail.com@8441c7b6-6410-7a6f-d5e6-c3b76e6ebf29>
2011-11-15 15:33:30 +0000

    info de brillo, entropia, etc. inicio de transformaciones lineales

--- trunk/imagen.h
+++ trunk/imagen.h
@@ -11,32 +11,36 @@
 class Imagen {
 
 private:
-    QImage * image;
-    int * vect;
+    int * hist;
     int Mniveles; //numero total de niveles
     QString file;
     int err; //error: imagen null, o se usa un formato que no hemos albergado para vect
-    void createVect();
+    void createHist();
 
 public:
     Imagen();
     Imagen(QString fileName);
 
+    QImage * qimage;
+
     int error(); //se saca el error para que lo lea aShot y sace un MessageBox
     int M(); //numero total de niveles
     int max();
     int min();
-    int brillo();
-    int contraste();
-    int entropia();
+    double brillo();
+    double contraste();
+    double entropia();
 
+    double rel(int i); //frecuencia relativa del nivel i
     int height();
     int width();
     int size();
 
+    void update();
     //int pixel(int x, int y);
     //void setPixel(int value);
 
+    QImage qImage();
     QString fileName();
     QString extension();
     QString formato();
